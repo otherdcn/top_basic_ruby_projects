f3442882219e102b88f63e18c4318c41c2febe39
otherdcn <danny.c.ntwari@gmail.com>
2024-05-17 18:21:18 +0200

    Refactor caesar cipher

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,49 +1,56 @@
+require 'pry-byebug'
+
 def caesar_cipher(str,shift_factor)
 
   str_array = str.split("")
-  cipher_ordinal_array = []
+  cipher_array = []
 
-  str_array.each do |letter|
+  str_array.each do |character|
     # Using ordinal number to shift
-    if letter.ord.between?(48,57) # Shift numbers 0 - 9
-      char = letter.ord + shift_factor
+    if character.ord.between?(48,57) # Shift numbers 0 - 9
+      char = character.ord + shift_factor
 
       # Ensure to wrap from 9 to 0
       until char <= 57
-        char = (char - 57) + 47
+        char -= 10
       end
 
-      cipher_ordinal_array.push(char)
-    elsif letter.ord.between?(65,90) # Shift uppercase letters
-      char = letter.ord + shift_factor
+      cipher_array.push(char.chr)
+    elsif character.ord.between?(65,90) # Shift uppercase characters
+      char = character.ord + shift_factor
 
       # Ensure to wrap from Z to A
       until char <= 90
-        char = (char - 90) + 64
+        char -= 26
       end
 
-      cipher_ordinal_array.push(char)
-    elsif letter.ord.between?(97,122) # Shift lowercase letters
-      char = letter.ord + shift_factor
+      cipher_array.push(char.chr)
+    elsif character.ord.between?(97,122) # Shift lowercase characters
+      char = character.ord + shift_factor
 
       # Ensure to wrap from z to a
       until char <= 122
-        char = (char - 122) + 96
+        char -= 26
       end
 
-      cipher_ordinal_array.push(char)
+      cipher_array.push(char.chr)
     else # Keep same ordinal place for every other charactor in string
-      cipher_ordinal_array.push(letter.ord)
+      cipher_array.push(character)
     end
   end
 
-  cipher_chars_array = cipher_ordinal_array.map { |ord| ord.chr }
-  cipher_str = cipher_chars_array.join
+  cipher_str = cipher_array.join
+  cipher_str
 end
 
 str = "What a string!"
-shift_factor = 3
+shift_factor = 100
 cipher_str = caesar_cipher(str,shift_factor)
+puts "String: #{str} ===> Shift #{shift_factor}"
+puts "Cipher: #{cipher_str}"
 
+str = "1 2 3 4 5"
+shift_factor = 15
+cipher_str = caesar_cipher(str,shift_factor)
 puts "String: #{str} ===> Shift #{shift_factor}"
 puts "Cipher: #{cipher_str}"
